a0f784be0d4045ec61e79076affb54a2762a7f91
moskize <xiangtantaozeyu@icloud.com>
2016-05-18 18:53:37 +0800

    init

---
+++ PLPlayerTrainingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PLPlayerTrainingDemo
+//
+//  Created by TaoZeyu on 16/5/18.
+//  Copyright © 2016年 taozeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
