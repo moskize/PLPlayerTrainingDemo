20e63344f09b134878ab411630ac2f02efac8fe2
moskize <xiangtantaozeyu@icloud.com>
2016-05-18 19:17:54 +0800

    add a simple demo.

--- PLPlayerTrainingDemo/ViewController.m
+++ PLPlayerTrainingDemo/ViewController.m
@@ -7,21 +7,26 @@
 //
 
 #import "ViewController.h"
+#import <PLPlayerKit/PLPlayer.h>
 
-@interface ViewController ()
-
+@interface ViewController () <PLPlayerDelegate>
+@property (nonatomic, strong) PLPlayer *player;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    PLPlayerOption *option = [PLPlayerOption defaultOption];
+    
+    NSURL *url = [NSURL URLWithString:@"rtmp://pili-live-rtmp.pilitest.qiniucdn.com/pilitest/solution1"];
+    self.player = [PLPlayer playerWithURL:url option:option];
+    self.player.delegate = self;
+    
+    [self.view addSubview:self.player.playerView];
+    
+    [self.player play];
 }
 
 @end
